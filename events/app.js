e2188a91a9bc630be4b490463570f293ebe5a84b
hamid2117 <saim008865@gmail.com>
2023-06-18 12:54:53 +0500

    learning events

--- events/app.js
+++ events/app.js
@@ -2,8 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-    };
+    }
   },
-});
+  methods: {
+    add() {
+      counter++
+    },
+    remove() {
+      counter--
+    },
+  },
+})
 
-app.mount('#events');
+app.mount('#events')
