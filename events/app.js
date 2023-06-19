106816cf42ed408445c426f8fecf62b980af20e9
hamid2117 <saim008865@gmail.com>
2023-06-19 05:11:35 +0500

    done implementing watcher

--- events/app.js
+++ events/app.js
@@ -1,15 +1,34 @@
+//without event why functions in method why call again if we trigger any event
 const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
     }
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.name} Mehmood`
+    },
   },
   methods: {
     add() {
-      counter++
+      this.counter++
     },
     remove() {
-      counter--
+      this.counter--
+      console.log('this runing')
+    },
+    confirmName() {
+      this.confirmedName = this.name
     },
   },
 })
