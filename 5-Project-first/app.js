df6e12d20744d79783a30d0b7c10a86d767dcfbb
hamid2117 <saim008865@gmail.com>
2023-06-20 06:25:22 +0500

    done adding resetgame , surender game

--- 5-Project-first/app.js
+++ 5-Project-first/app.js
@@ -70,6 +70,15 @@ const app = Vue.createApp({
       }
       this.attackPlayer()
     },
+    resetGame() {
+      this.playerHealth = 100
+      this.monsterHealth = 100
+      this.currentRound = 0
+      this.winner = null
+    },
+    surrenderGame() {
+      this.winner = 'monster'
+    },
   },
 })
 
