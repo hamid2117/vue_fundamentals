83319450061df7834f057f59a9e4828022f9f3f8
hamid2117 <saim008865@gmail.com>
2023-07-05 10:30:25 +0500

    done adding UI as global

--- 6-rememberme-project/src/main.js
+++ 6-rememberme-project/src/main.js
@@ -1,9 +1,15 @@
 import './assets/main.css'
 import BaseCard from './components/UI/BaseCard.vue'
+import BaseButton from './components/UI/BaseButton.vue'
+import BaseDailog from './components/UI/BaseDailog.vue'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
+
 app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
+app.component('base-dailog', BaseDailog)
 
 app.mount('#app')
