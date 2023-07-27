b1bb41902a8218189653b9b2e4a6a174b5309e73
hamid2117 <saim008865@gmail.com>
2023-07-28 00:35:01 +0500

    updating base search

--- 7-composition/src/components/UI/BaseSearch.vue
+++ 7-composition/src/components/UI/BaseSearch.vue
@@ -1,10 +1,15 @@
 <template>
   <form>
-    <input type="search" @input="search" :value="searchTerm" placeholder="Filter items" />
+    <input
+      type="search"
+      @input="search"
+      :value="searchTerm"
+      placeholder="Filter items"
+    />
   </form>
 </template>
 
-<script>
+<script setup>
 export default {
   props: ['searchTerm'],
   emits: ['search'],
